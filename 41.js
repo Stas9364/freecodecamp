36bb9868bd8d9d6f82f15e999a7a969dc052adff
Stanislav Lisovskiy <86848801+Stas9364@users.noreply.github.com>
2022-03-05 16:24:04 +0300

    Add files via upload

---
+++ 41.js
@@ -0,0 +1,7 @@
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line
+let a = ['Paul', 35];
+myArray.unshift(a);
---
+++ 42.js
@@ -0,0 +1,8 @@
+const myList = [
+    ['candy', 22], 
+    ['bred', 10],
+    ['chocolat', 44],
+    ['gold', 75],
+    ['ham', 12]
+    ];
+  
